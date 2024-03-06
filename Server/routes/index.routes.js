cff37932441da0a0b61b412e9b23432dae36f451
Agustín Gurvich <agus.gurvich@gmail.com>
2024-03-06 14:44:16 -0300

    First Commit

---
+++ Server/routes/index.routes.js
@@ -0,0 +1,24 @@
+import {Router} from 'express';
+import { pool } from '../db.js';
+import auth from '../lib/auth.js';
+const router = Router();
+
+router.get("/cancion/para/mi/muerte", async (req, res) => {
+    res.render('ingresoControl');
+});
+
+router.get('/control', auth.isLinea ,(req, res) => {
+    res.render('control');
+});
+
+router.get('/crearBecario', (req, res) => {
+    res.render('crearBecario');
+});
+
+
+
+// Tambien podriamos usar
+//[rows] paara devolver directamente las filas
+
+
+export default router;
